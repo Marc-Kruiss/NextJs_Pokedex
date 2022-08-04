87d75ee2a4328df4f313a6ed0417f347dc7597f4
Marc Kruiß <k.marc@a1.net>
2022-08-04 22:12:28 +0200

    debugger setup + configuration stuff

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images:{
     domains:['assets.pokemon.com','raw.githubusercontent.com']
-  }
+  },
+
 }
 
 module.exports = nextConfig

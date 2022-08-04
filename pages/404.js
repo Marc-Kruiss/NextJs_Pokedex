4afb6b00ca5aed5d4c18be1de58a8dbb2729d3c1
Marc Kruiß <k.marc@a1.net>
2022-08-04 22:12:43 +0200

    update 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 function NotFound() {
   return (
-    <>
-        <h1>Page not found</h1>
-    </>
-  )
+    <div className='grid place-items-center h-screen items-center"'>
+      <Image src={"/blackedPokemon.png"} height={500} width={500} />
+      <h1 className="absolute text-center translate-y-52 text-9xl font-mono">
+        Page Not Found
+      </h1>
+      <button className="bg-gray-800 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 rounded-3xl p-3">
+      <a href={"/"} className="text-4xl">
+          Home 
+        </a>
+      </button>
+    </div>
+  );
 }
 
-export default NotFound
+export default NotFound;

52d15da35d9327171149e3b355ce31838bf3d94a
Marc Kruiß <k.marc@a1.net>
2022-08-19 15:24:46 +0200

    api call handels search term and filters list

---
+++ pages/api/[searchTerm].tsx
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { PokeSearchApiRequest } from "../../components/types/ApiInterfaces";
+import { IPokemonUrl } from "../../components/types/PokemonInterfaces";
+
+export default async function handler(
+  req: PokeSearchApiRequest,
+  res: NextApiResponse
+) {
+  const { searchTerm } = req.query;
+  const maxResults: number = 15;
+
+  if (req.method === "GET" && searchTerm) {
+    const pokemonsResponse = await fetch(
+      "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
+    );
+    const pokemonNames: IPokemonUrl[] = await pokemonsResponse
+      .json()
+      .then((data) => {
+        return data.results;
+      });
+
+    const filteredPokemons: IPokemonUrl[] = pokemonNames
+      .map((p, index) => ({ name: p.name, url: p.url, id: index }))
+      .filter((p) =>
+        p.name.toLowerCase().startsWith(String(searchTerm).toLowerCase())
+      );
+
+    res.end(JSON.stringify({ filteredPokemons: filteredPokemons }));
+  }
+}

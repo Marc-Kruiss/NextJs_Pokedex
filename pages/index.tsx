3ee08e248f5d96896533998988aee0ab455501de
Marc Kruiß <k.marc@a1.net>
2022-08-19 15:25:02 +0200

    implement searchbar with timeout-auto-search

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,28 @@
 import Layout from "../components/Layout";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Pokemon from "../components/Pokemon";
 import { GetStaticProps } from "next";
 import { PokemonListResponse } from "../components/types/PokemonInterfaces";
 import { mapPokemonListResponse } from "../components/helper/mapper";
 import { AiOutlineSearch } from "react-icons/ai";
+import { PokeSearchApiRequest } from "../components/types/ApiInterfaces";
 
 export default function Home({
   initialPokemon,
 }: {
   initialPokemon: PokemonListResponse;
 }) {
+  const [searchInput, setSearchInput] = useState("");
   const [pokemon, setPokemon] = useState<PokemonListResponse>(initialPokemon);
   const [offset, setOffset] = useState(0);
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      filterPokemons(searchInput);
+    }, 1500);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [searchInput]);
 
   const fetchPokemon = async (url: string, isNext: boolean) => {
     const response = await fetch(url);
@@ -24,16 +34,60 @@ export default function Home({
     setPokemon(nextPokemon);
   };
 
+  const filterPokemons = async (searchTerm: string) => {
+    const response = await fetch(`/api/${searchTerm}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const filteredPokemons = await response.json().then((data) => {
+      return data.filteredPokemons;
+    });
+
+    const data: PokemonListResponse = {
+      nextUrl: "",
+      previousUrl: "",
+      pokemonList: filteredPokemons,
+    };
+    setPokemon(data);
+  };
+
+  const getIdByName = async (name: string) => {
+    const response = await fetch(`/api/getId/${name}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const pokemonId = await response.json().then((data) => {
+      return data.pokemonId;
+    });
+
+    return pokemonId;
+  };
+
   return (
     <Layout title={"PokeDex"}>
       <div className="flex flex-col justify-center">
-        <AiOutlineSearch className="absolute left-7 lg:left-14 text-black size text-xl"/>
-        <input type="text" value={"sef"} className="text-gray-700 rounded-md px-16 py-2 mx-5 bg-slate-300" />
+        <AiOutlineSearch className="absolute left-7 lg:left-14 text-black size text-xl" />
+        <input
+          type="text"
+          className="text-gray-700 rounded-md px-16 py-2 mx-5 bg-slate-300"
+          placeholder="Search here..."
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-10 p-5">
         {pokemon.pokemonList.map((info, index) => (
-          <Pokemon name={info.name} key={index} index={offset + index} />
+          <Pokemon
+            name={info.name}
+            key={index}
+            index={info.id ? info.id : offset + index}
+          />
         ))}
       </div>
 

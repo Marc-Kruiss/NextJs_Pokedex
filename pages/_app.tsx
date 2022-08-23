1dee1803ab3b7fd28f1445c9abbc7aaa43edd048
Marc Kruiß <k.marc@a1.net>
2022-08-23 22:08:33 +0200

    Use Language Provider on each page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { AppProps } from "next/app";
 import "../styles/globals.css";
+import { GetStaticProps } from "next";
+import { PokemonListResponse } from "../components/types/PokemonInterfaces";
+import { mapPokemonListResponse } from "../components/helper/mapper";
+import { LanguageProvider } from "../context/Language/LanguageContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <LanguageProvider>
+        <Component {...pageProps} />
+      </LanguageProvider>
+    </>
+  );
 }
 
 export default MyApp;
